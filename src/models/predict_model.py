7058103a7b2f78734f9612af4c292e5ef1c0b777
PabloPertusa <pablopertusa27@gmail.com>
2024-11-04 23:39:01 +0100

    volvemos

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,7 +1,21 @@
 import pickle
+import pandas as pd
+import json
 
 if __name__ == "__main__":
-    with open('random_forest_model.pkl', 'rb') as file:
+    with open('models/random_forest_model.pkl', 'rb') as file:
         loaded_model = pickle.load(file)
 
-    ind = []
+    with open('predictions/predictions_3.json', 'r') as f:
+        pred = json.load(f)
+
+    df = pd.read_csv('data/raw/transactions_data.csv')
+    d_pred = {}
+    for col in df.columns:
+        d_pred[col] = []
+
+    for id in pred['target']:
+        for col in df.columns:
+            d_pred[col].append(df[df['id'] == id][col].values)
+    print(d_pred)
+
